8e6bd6eab33276688f81fc286379e8afb7483116
Lorraine Gelis <lgelisdiaz@outlook.com>
2021-10-22 21:05:50 -0500

    Se realiza config firebase y data.json

---
+++ src/firebasekey.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCCKfC18HiDVMxY5_820rEqK4sL01v2pdg",
+    authDomain: "onboardingdigital-1f48f.firebaseapp.com",
+    projectId: "onboardingdigital-1f48f",
+    storageBucket: "onboardingdigital-1f48f.appspot.com",
+    messagingSenderId: "286988595771",
+    appId: "1:286988595771:web:4aefa7aa4b7b2e4ce5e21e",
+    measurementId: "G-3XS3HSQ39F"
+};
