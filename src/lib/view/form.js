8fb888d94ef1b365e36483f403df3adcbf2b6d61
Karen Poveda Lasprilla <karenpoveda29@gmail.com>
2021-10-23 12:29:07 -0500

    Agregar labels

--- src/lib/view/form.js
+++ src/lib/view/form.js
@@ -2,18 +2,38 @@ export const formInView = () => {
   const formSection = document.createElement("section");
   formSection.setAttribute('class', 'form-section');
   const formView = `
-  
-  <form id="formview3"> 
-  <input type="text"  name="fullName" id="fullName" placeholder="*Nombre(s) y Apellido(s)">
-  <input type="text"  name="docId"    id="docId"    placeholder="Documento de identidad">
-  <input type="text"  name="nit"      id="nit"      placeholder="NIT de la empresa">
-  <input type="text"  name="busiName" id="busiName" placeholder="Nombre de la empresa ">  
-  <input type="text"  name="phone"    id="phone"    placeholder="Número de teléfono">  
-  <input type="text"  name="city"     id="city"     placeholder="Ciudad y departamento">  
-  <input type="file"  name="file"      id="file"    multiple="multiple"> 
-  <button type="submit"> enviar </button>   
+  <form id="formview3" enctype="multipart/form-data">
+    <label for="fullName">Nombre completo representante legal</label>
+      <input type="text" name="fullName" id="fullName" value="${userName}" placeholder="Ejemplo: Ana María Mora Gil" disabled>
+    <label for="docId">Documento de identidad representante legal</label>  
+      <input type="text" name="docId" id="docId" value="${userId}" placeholder="Ejemplo: 1022564332" disabled>
+    <label for="nit">NIT de la empresa</label>  
+      <input type="text" name="nit" id="nit" value="${nitNumber}" placeholder="NIT de la empresa" disabled>
+    <label for="businessName">Nombre de la empresa</label>
+      <input type="text" name="busiName" id="businessName" value="${business}" placeholder="Ejemplo: Distribuidora Lorraine SAS" disabled>
+    <label for="phone">Número de teléfono</label>  
+      <input type="tel" name="phone" id="phone" placeholder="Ejemplo: 3119845632">
+    <label for="city">Ciudad y departamento</label>  
+      <input type="text" name="city" id="city" placeholder="Ejemplo: Bogotá/Cundinamarca">
+    <label for="file">Sube los documentos</label>
+      <input type="file" name="file" id="file" multiple="multiple"> 
+    <button type="submit"> enviar </button>   
   </form>
-`
-  formSection.innerHTML = formView;   
+  <p>Documentos requeridos:</p>
+    <ul>
+      <li>Cámara de Comercio</li>
+      <li>RUT</li>
+      <li>Documento de identidad</li>
+    <ul>
+  <p>Subir todos los documentos en formato PDF:</p>
+  `
+  formSection.innerHTML = formView; 
+   
   return formSection;
 };
+
+const userName = 'pepo perez';
+const userId = '1033742554';
+const nitNumber = '123456789';
+const business = 'Empresa sas';
+
