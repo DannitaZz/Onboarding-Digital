3b17bfffe0bbd96f72fe38ab74dd6b67a74dafcf
Karen Poveda Lasprilla <karenpoveda29@gmail.com>
2021-10-23 12:29:43 -0500

    Agregar labels, cambiar placeholders

--- src/lib/view/ingreso.js
+++ src/lib/view/ingreso.js
@@ -2,8 +2,20 @@ export const createLogInView = () => {
   const loginSection = document.createElement("section");
   loginSection.setAttribute('class', 'login-section');
   const loginView = `
-  <p> Vista 2 </p>
-`
+  <form id="formview2"> 
+    <label for="fullName">Nombre completo representante legal</label>
+      <input type="text" name="fullName" id="fullName" placeholder="Ejemplo: Ana María Mora Gil">
+    <label for="docId">Documento de identidad representante legal</label>  
+      <input type="text" name="docId" id="docId" placeholder="Ejemplo: 1022564332">
+    <label for="nit">NIT de la empresa</label>  
+      <input type="text" name="nit" id="nit" placeholder="Sin dígito de verificación">
+    <label for="businessName">Nombre de la empresa</label>
+      <input type="text"  name="busiName" id="businessName" placeholder="Ejemplo: Distribuidora Lorraine SAS">
+ 
+    <button type="submit"> enviar </button>   
+ 
+  </form>
+  `
   loginSection.innerHTML = loginView;
 
   
