d0b554c3ececc7774c36af7b5189df75a4719dac
Karen Poveda Lasprilla <karenpoveda29@gmail.com>
2021-10-23 16:13:32 -0500

    modificar template

--- src/lib/view/info.js
+++ src/lib/view/info.js
@@ -2,13 +2,15 @@ export const infoInView = () => {
   const infoSection = document.createElement('section');
   infoSection.setAttribute('class', 'info-section');
   const loginSection = `
-    <p>Abre una cuenta para tu PYME de forma digital,</br> fácil y sencilla.</p>
-    <p>*Requisitos en formato PDF para continuar el proceso:</br>
-    -Cámara de comercio</br>
-    -RUT</br>
-    -Cédula del representante legal</p>
+    <p>Abre una cuenta para tu PYME de forma digital, fácil y sencilla.</p>
+    <p>*Requisitos en formato PDF para continuar el proceso:</p>
+    <ul>
+      <li>Cámara de comercio</li>
+      <li>RUT</li>
+      <li>Cédula del representante legal</li>
+    </ul>
     <button class="btn" id="btnEnter">INGRESA AQUÍ</button>
-`;
+  `;
   infoSection.innerHTML = loginSection;
   return infoSection;
 };
