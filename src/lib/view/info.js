9da995e6da5ff27a63f6ed2298b495fa1bb98321
Lu Valcke <lfvalcke@gmail.com>
2021-10-23 12:20:18 -0500

    Se hace maqueta de template para primera vista

--- src/lib/view/info.js
+++ src/lib/view/info.js
@@ -2,8 +2,13 @@ export const infoInView = () => {
   const infoSection = document.createElement('section');
   infoSection.setAttribute('class', 'info-section');
   const loginSection = `
-  <p> Vista 1 </p>
-`
+    <p>Abre una cuenta para tu PYME de forma digital,</br> fácil y sencilla.</p>
+    <p>*Requisitos en formato PDF para continuar el proceso:</br>
+    -Cámara de comercio</br>
+    -RUT</br>
+    -Cédula del representante legal</p>
+    <button class="btn" id="btnEnter">INGRESA AQUÍ</button>
+`;
   infoSection.innerHTML = loginSection;
   return infoSection;
 };
