06c101c214ceba078fd58ab02d3d45814b393c5b
leidy acevedo <leidyjuliethaj@ufps.edu.co>
2021-10-23 10:40:05 -0500

    agregar html template de la vista 3

--- src/lib/view/form.js
+++ src/lib/view/form.js
@@ -3,10 +3,18 @@ export const formInView = () => {
   const formSection = document.createElement("section");
   formSection.setAttribute('class', 'form-section');
   const formView = `
-  <p> Vista 3 </p>
-`
-  formSection.innerHTML = formView;
-
   
+  <form id="formview3"> 
+  <input type="text"  name="fullName" id="fullName" placeholder="*Nombre(s) y Apellido(s)">
+  <input type="text"  name="docId"    id="docId"    placeholder="Documento de identidad">
+  <input type="text"  name="nit"      id="nit"      placeholder="NIT de la empresa">
+  <input type="text"  name="busiName" id="busiName" placeholder="Nombre de la empresa ">  
+  <input type="text"  name="phone"    id="phone"    placeholder="Número de teléfono">  
+  <input type="text"  name="city"     id="city"     placeholder="Ciudad y departamento">  
+  <input type="file"  name="file"      id="file"    multiple="multiple"> 
+  <button type="submit"> enviar </button>   
+  </form>
+`
+  formSection.innerHTML = formView;   
   return formSection;
 };
