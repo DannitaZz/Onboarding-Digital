d645135fdaa8b0dd0a5c24acbcb3623b3c63d5e4
Daniela Funes <danielafunes1970@gmail.com>
2021-10-24 00:26:47 -0500

    Agregar clases para estilo

--- src/lib/view/msg.js
+++ src/lib/view/msg.js
@@ -2,8 +2,7 @@ export const messageInView = () => {
   const messageSection = document.createElement("section");
   messageSection.setAttribute('class', 'message-section');
   const messageView = `
-    <p>Tu solicitud ha sido enviada con éxito. En los próximos días hábiles recibirás al correo electrónico información sobre tu cuenta.</p>
-    <button class="btn" id="btnEnter">INGRESA AQUÍ</button>
+    <p class="message">Tu solicitud ha sido enviada con éxito. En los próximos días hábiles recibirás al correo electrónico información sobre tu cuenta.</p>
   `;
   messageSection.innerHTML = messageView;
   return messageSection;
