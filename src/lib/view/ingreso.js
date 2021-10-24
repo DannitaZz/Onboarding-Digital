c182750794802b10bc8cd823207ac32aeedd89ab
Daniela Funes <danielafunes1970@gmail.com>
2021-10-24 01:28:49 -0500

    Modificar template HTML

--- src/lib/view/ingreso.js
+++ src/lib/view/ingreso.js
@@ -2,17 +2,18 @@ export const createLogInView = () => {
   const loginSection = document.createElement("section");
   loginSection.setAttribute('class', 'login-section');
   const loginView = `
+  <h1>Formulario para verificación de identidad</h1>
   <form id="formview2"> 
     <label for="fullName">Nombre completo representante legal</label>
       <input type="text" name="fullName" id="fullName" placeholder="Ejemplo: Ana María Mora Gil">
-    <label for="docId">Documento de identidad representante legal</label>  
+    <label for="docId">Documento de identidad</label>  
       <input type="text" name="docId" id="docId" placeholder="Ejemplo: 1022564332">
     <label for="nit">NIT de la empresa</label>  
       <input type="text" name="nit" id="nit" placeholder="Sin dígito de verificación">
     <label for="businessName">Nombre de la empresa</label>
-      <input type="text"  name="busiName" id="businessName" placeholder="Ejemplo: Distribuidora Lorraine SAS">
+      <input type="text" name="busiName" id="businessName" placeholder="Ejemplo: Distribuidora Lorraine SAS">
  
-    <button type="submit"> enviar </button>   
+    <button type="submit" class="btn-send">ENVIAR</button>   
  
   </form>
   `
@@ -21,4 +22,3 @@ export const createLogInView = () => {
   
   return loginSection;
 };
-
