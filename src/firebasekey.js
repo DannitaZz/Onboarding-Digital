3c5c9911a5e9c0c6de50c4a233f9f5e78765e615
leidy acevedo <leidyjuliethaj@ufps.edu.co>
2021-10-24 02:54:14 -0500

    agregar inicializacion de firebase

--- src/firebasekey.js
+++ src/firebasekey.js
@@ -1,4 +1,5 @@
 const firebaseConfig = {
+
     apiKey: "AIzaSyCCKfC18HiDVMxY5_820rEqK4sL01v2pdg",
     authDomain: "onboardingdigital-1f48f.firebaseapp.com",
     projectId: "onboardingdigital-1f48f",
@@ -7,3 +8,6 @@ const firebaseConfig = {
     appId: "1:286988595771:web:4aefa7aa4b7b2e4ce5e21e",
     measurementId: "G-3XS3HSQ39F"
 };
+
+
+firebase.initializeApp(firebaseConfig);
