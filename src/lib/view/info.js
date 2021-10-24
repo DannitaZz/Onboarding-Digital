e98ac812a24ca4a3cd2f8bf178a9351c94926883
Daniela Funes <danielafunes1970@gmail.com>
2021-10-23 20:37:28 -0500

    Agregar clases para dar estilos

--- src/lib/view/info.js
+++ src/lib/view/info.js
@@ -2,9 +2,9 @@ export const infoInView = () => {
   const infoSection = document.createElement('section');
   infoSection.setAttribute('class', 'info-section');
   const loginSection = `
-    <p>Abre una cuenta para tu PYME de forma digital, fácil y sencilla.</p>
-    <p>*Requisitos en formato PDF para continuar el proceso:</p>
-    <ul>
+    <p class="bienvenida" >Abre una cuenta para tu PYME de forma digital, fácil y sencilla.</p>
+    <p class="requisitos">*Requisitos en formato PDF para continuar el proceso:</p>
+    <ul class="listaRequisitos">
       <li>Cámara de comercio</li>
       <li>RUT</li>
       <li>Cédula del representante legal</li>
